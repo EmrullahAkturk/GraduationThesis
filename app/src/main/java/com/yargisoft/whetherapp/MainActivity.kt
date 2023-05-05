6317f003535f0c8b393bc5fd32b73eeb6704d2c1
Emrullah Aktürk <akturk.emrullah@hotmail.com>
2023-05-05 14:44:37 +0300

    First Commit

---
+++ app/src/main/java/com/yargisoft/whetherapp/MainActivity.kt
@@ -0,0 +1,11 @@
+itpackage com.yargisoft.whetherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
